7bf8af2656fa6b81b6f3c4bad7c81249c49361b6
FoxUnderGround <54197021+FoxUnderGround@users.noreply.github.com>
2021-06-01 09:25:32 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var http = require('http')
+const fetch = require("node-fetch");
+var app = express();
+var count = 0;
+
+app.set("view engine", "ejs")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+	res.render("index");
+})
+
+
+app.post('/getweather', function (req, res) {
+	let weather_data;
+	let data_buffer =[];
+	var buffer;
+	//console.log(req.body);
+	http.get(`http://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.lon}&units=metric&appid=235a00b3c6dc62cc5cd17446e4ac2fc9`, 
+	(response) => {
+		if (response.statusCode === 200) {
+		  response.on("data", (data) => {
+			data_buffer.push(data);
+		  }
+		 ).on('end', () => {
+		 buffer = Buffer.concat(data_buffer);
+        	weather_data =  buffer.toString();
+			console.log(weather_data);
+			res.send(weather_data);
+		}
+		 )}
+	}
+	);
+  })
+
+var server = app.listen(8081, function () {
+	var host = server.address().address
+	var port = server.address().port
+	console.log(`Example app listening at http://${host}:${port}`)
+})
+
