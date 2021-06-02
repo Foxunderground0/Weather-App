2c827dbf22f4a89491ccdc1bd09bf6740125a5d8
FoxUnderGround <54197021+FoxUnderGround@users.noreply.github.com>
2021-06-02 18:10:59 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('http')
 var app = express();
-var count = 0;
+var apikey =""
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+  })
+
+  readline.question(`Insert API key:`, (key) => {
+	apikey = key;
+	console.log(`Key Recived: ${key}`);
+	readline.close()
+  })
 
 app.set("view engine", "ejs")
 
@@ -22,7 +32,7 @@ app.post('/getweather', function (req, res) {
 	let data_buffer =[];
 	var buffer;
 	console.log(req.body);
-	http.get(`http://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.lon}&units=metric&appid=235a00b3c6dc62cc5cd17446e4ac2fc9`, 
+	http.get(`http://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.lon}&units=metric&appid=${apikey}`, 
 	(response) => {
 		if (response.statusCode === 200) {
 		  response.on("data", (data) => {
@@ -38,10 +48,11 @@ app.post('/getweather', function (req, res) {
 	}
 	);
   })
+  
 let port = process.env.PORT || 8081;
 var server = app.listen(port, function () {
 	var host = server.address().address
 	var port = server.address().port
-	console.log(`Example app listening at http://${host}:${port}`)
+	//console.log(`Example app listening at http://${host}:${port}`)
 })
 
