288438069ece682dedf20c900c62c32bf9893f35
Foxunderground <umerirfan1205@gmail.com>
2021-07-08 10:37:38 +0500

    Added api key in .env

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var http = require('http')
+require("dotenv").config();
+var express = require("express");
+var bodyParser = require("body-parser");
+var http = require("http");
 var app = express();
-var apikey =""
+var apikey = process.env.APIKEY;
 /* 
 const readline = require('readline').createInterface({
 	input: process.stdin,
@@ -15,45 +16,45 @@ const readline = require('readline').createInterface({
 	readline.close()
   })
 */
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
 	res.render("index");
-})
+});
 
-
-app.post('/getweather', function (req, res) {
+app.post("/getweather", function (req, res) {
 	let weather_data;
-	let data_buffer =[];
+	let data_buffer = [];
 	var buffer;
 	console.log(req.body);
-	http.get(`http://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.lon}&units=metric&appid=${apikey}`, 
-	(response) => {
-		if (response.statusCode === 200) {
-		  response.on("data", (data) => {
-			data_buffer.push(data);
-		  }
-		 ).on('end', () => {
-		 buffer = Buffer.concat(data_buffer);
-        	weather_data =  buffer.toString();
-			//console.log(weather_data);
-			res.send(weather_data);
+	http.get(
+		`http://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.lon}&units=metric&appid=${apikey}`,
+		(response) => {
+			if (response.statusCode === 200) {
+				response
+					.on("data", (data) => {
+						data_buffer.push(data);
+					})
+					.on("end", () => {
+						buffer = Buffer.concat(data_buffer);
+						weather_data = buffer.toString();
+						//console.log(weather_data);
+						res.send(weather_data);
+					});
+			}
 		}
-		 )}
-	}
 	);
-  })
+});
 
 let port = process.env.PORT || 8081;
 var server = app.listen(port, function () {
-	var host = server.address().address
-	var port = server.address().port
+	var host = server.address().address;
+	var port = server.address().port;
 	//console.log(`Example app listening at http://${host}:${port}`)
-})
-
+});
